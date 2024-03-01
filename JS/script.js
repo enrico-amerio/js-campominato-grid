639353d3765a47e1b3d5fcb5fbd9e0ee170f0b30
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-01 10:12:50 +0100

    [fix] counter box in console onClick

--- JS/script.js
+++ JS/script.js
@@ -6,28 +6,30 @@ startBtn.addEventListener('click', function(){
   grid.innerHTML='';
   document.querySelector('.d-none').classList.remove('d-none');
   for (let i = 0; i < 100; i++) {
-    const box = genBox();
+    const box = genBox([i]);
     grid.append(box);
-
+    
     box.addEventListener('click', function(){
       box.classList.toggle('clicked');
+      console.log(box._boxNum);
     })
     difficultyOptions.addEventListener('change', function() {
       if(this.value === 'easy'){
         box.classList.add('easy');
-        box.classList.remove('hard', 'normal');
+        box.classList.remove('hard', 'normal', 'clicked');
+        
         
       }else if(this.value === 'normal'){
         box.classList.add('normal');
-        box.classList.remove('hard', 'easy');
+        box.classList.remove('hard', 'easy', 'clicked');
         
       }else{
         box.classList.add('hard');
-        box.classList.remove('normal', 'easy');
+        box.classList.remove('normal', 'easy','clicked');
     
       };
     } );
-  }
+  };
   
 });
 
@@ -35,9 +37,10 @@ startBtn.addEventListener('click', function(){
 
 
 
-function genBox () {
+function genBox([i]) {
   const box = document.createElement("div");
   box.classList.add('box', 'hard');
+  box._boxNum = i + 1;
   return box;
 
 }
