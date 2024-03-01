1c10a46615128d8c88d81777d08252fe62da3b7b
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-01 12:27:33 +0100

    [fix] Now i generate the right amount of boxes

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,28 @@
 const grid = document.querySelector('.grid');
 const startBtn = document.getElementById('start-btn');
 const difficultyOptions = document.getElementById('difficulty');
+const levels = [100, 81, 49]
 
 startBtn.addEventListener('click', function(){
   grid.innerHTML='';
-  document.querySelector('.d-none').classList.remove('d-none');
-  for (let i = 0; i < 100; i++) {
+  const cellNumbers = levels[difficultyOptions.value];
+  const difficulty = difficultyOptions.value;
+  console.log(difficulty);
+  for (let i = 0; i < cellNumbers; i++) {
     const box = genBox([i]);
-    grid.append(box);
-    
-    box.addEventListener('click', function(){
-      box.classList.toggle('clicked');
-      console.log(box._boxNum);
-    })
-    difficultyOptions.addEventListener('change', function() {
-      if(this.value === 'easy'){
+      if(difficulty == 0){
         box.classList.add('easy');
-        box.classList.remove('hard', 'normal', 'clicked');
-        
-        
-      }else if(this.value === 'normal'){
+      }else if(difficulty == '1'){
         box.classList.add('normal');
-        box.classList.remove('hard', 'easy', 'clicked');
-        
       }else{
         box.classList.add('hard');
-        box.classList.remove('normal', 'easy','clicked');
-    
       };
-    } );
+    grid.append(box);
+    box.addEventListener('click', function(){
+      box.classList.add('clicked');
+      console.log(box._boxNum);
+    })
   };
-  
 });
 
 
@@ -39,7 +31,7 @@ startBtn.addEventListener('click', function(){
 
 function genBox([i]) {
   const box = document.createElement("div");
-  box.classList.add('box', 'hard');
+  box.classList.add('box');
   box._boxNum = i + 1;
   return box;
 
